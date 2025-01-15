d820c1cc5d732c5bc69f4dda437a27737161918c
Andre Gois <andresousalee@gmail.com>
2025-01-14 21:56:20 -0300

    :sparkles: feat: projeto consulta paginada

---
+++ App11-JdbcPaginada/src/main/java/com/springbatch/arquivolargurafixa/reader/JdbcCursorReaderConfig.java
@@ -0,0 +1,34 @@
+package com.springbatch.arquivolargurafixa.reader;
+
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+import com.springbatch.arquivolargurafixa.dominio.Cliente;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class JdbcCursorReaderConfig {
+
+	@Bean
+	public JdbcCursorItemReader<Cliente> jdbcCursorReader(
+			@Qualifier("appDataSource") DataSource dataSource
+	) {
+		return new JdbcCursorItemReaderBuilder<Cliente>()
+				.name("JdbcCursorItemReader")
+				.dataSource(dataSource)
+				.sql("select c.* from Cliente c")
+				.rowMapper(new BeanPropertyRowMapper<>(Cliente.class))
+				.build();
+	}
+}
